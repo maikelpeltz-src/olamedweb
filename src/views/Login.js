6bdf46017852ad31768f7a1f4ca0a6073411d93e
fernandosalvettisrc <fernando.salvetti@srcsistemas.com.br>
2020-07-21 09:49:41 -0300

    Alteração da tela de login

--- src/views/Login.js
+++ src/views/Login.js
@@ -8,9 +8,13 @@ import '../assets/scss/style.scss';
 import Aux from "../hoc/_Aux";
 import Breadcrumb from "../App/layout/AdminLayout/Breadcrumb";
 import NotificationSystem from 'react-notification-system';
-
+import Logo from '../assets/images/logo.png';
+import './css/login.css';
 var notificacaoLoginErrado = '';
 notificacaoLoginErrado = React.createRef()
+
+var notificacaoFuncionalidadeNaoDesenvolvida = '';
+notificacaoFuncionalidadeNaoDesenvolvida = React.createRef()
 
 class Login extends React.Component {
     constructor(props) {
@@ -35,6 +39,16 @@ class Login extends React.Component {
             autoDismiss: 0
         });
     }
+    addNotificationFuncionalidadeNaoDesenvolvida(event) {
+        event.preventDefault();
+        const notification = notificacaoFuncionalidadeNaoDesenvolvida.current;
+        notification.addNotification({
+            message: 'Funcionalidade não desenvolvida',
+            level: 'warning',
+            position: 'tr',
+            autoDismiss: 0
+        });
+    }
 
     componentDidMount() {
 
@@ -54,11 +68,6 @@ class Login extends React.Component {
             [field]: value
         });
     }
-
-    mensagemValidacaoErro() {
-        document.getElementById("messagemUI").click();
-    }
-
 
     getMessageByErrorCode(errorCode) {
         switch (errorCode) {
@@ -96,10 +105,7 @@ class Login extends React.Component {
         return (
             <Aux>
                 <NotificationSystem ref={notificacaoLoginErrado} style={style} />
-                {this.state.erroLogin > 0 ?
-                    this.mensagemValidacaoErro()
-                    : null
-                }
+                <NotificationSystem ref={notificacaoFuncionalidadeNaoDesenvolvida} style={style} />
                 <Breadcrumb />
                 <div className="auth-wrapper">
                     {this.state.usuarioLogado > 0 ? <Redirect to='/dashboard' /> : null}
@@ -113,10 +119,9 @@ class Login extends React.Component {
                         <div className="card">
                             <div className="card-body text-center">
                                 <div className="mb-4">
-                                    <i className="feather icon-unlock auth-icon" />
+                                    <img src={Logo} alt="Dynamic logo" className = "login-page-logo mb-3"/>
                                 </div>
-                                <h3 className="mb-4">Login</h3>
-                                <div className="input-group mb-3">
+                                <div className="input-group mb-3 mt-4">
                                     <input type="email" onChange={(e) => this.onChangeHandler('email', e.target.value)} className="form-control" placeholder="Email" />
                                 </div>
                                 <div className="input-group mb-4">
@@ -125,12 +130,12 @@ class Login extends React.Component {
                                 <div className="form-group text-left">
                                     <div className="checkbox checkbox-fill d-inline">
                                         <input type="checkbox" name="checkbox-fill-1" id="checkbox-fill-a1" />
-                                        <label htmlFor="checkbox-fill-a1" className="cr"> Save credentials</label>
+                                        <label htmlFor="checkbox-fill-a1" className="cr cr-credenciais"> Salvar credenciais</label>
                                     </div>
                                 </div>
-                                <button className="btn btn-primary shadow-2 mb-4" onClick={() => this.tryLogin()}>Login</button>
-                                <p className="mb-2 text-muted">Forgot password? <NavLink to="/auth/reset-password-1">Reset</NavLink></p>
-                                <p className="mb-0 text-muted">Don’t have an account? <NavLink to="/auth/signup-1">Signup</NavLink></p>
+                                <button className="btn btn-login shadow-2 mb-4" onClick={() => this.tryLogin()}>Login</button>
+                                <p className="mb-2 text-muted p-color">Esqueceu a senha? <a onClick = {(e) => (this.addNotificationFuncionalidadeNaoDesenvolvida(e))}>Redefinir senha</a></p>
+                                <p className="mb-0 text-muted p-color">Não possuí uma conta? <NavLink to="/auth/signup-1">Criar conta</NavLink></p>
                             </div>
                         </div>
                     </div>
